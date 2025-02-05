8319ae4db8969d1cc2708a63ca3ecd5badd9a870
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-02-06 00:06:47 +0300

    Day 1 Project: Band Name Generator
    This repository contains a simple Python project called Band Name Generator, part of Emre Uslu's 100-day Python project streak. The script takes user input to generate a creative band name and features a fun surprise ASCII art.

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Il s'agit de la première application Python réalisée par Emre Uslu.")
+print("Bienvenue sur le générateur de noms de groupes!")
+
+city = input("Entrez la ville d'où vous venez: \n")
+pet = input("Entrez le nom de votre animal: \n")
+
+print("Le nom de votre groupe est: " + city + " " + pet + ".")
+love='Jaime Ecem';print('\n'.join([''.join([(love[(x-y)%len(love)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ') for x in range(-30,30 )]) for y in range(15, -15, -1)]))
